caeb1778a0ca629dc45eedbf6eb96d96fb19f1f9
Dion <dionschippers@gmail.com>
2025-11-12 11:22:09 +0100

    add working memory for mlx-community/Llama-3.2-3B-Instruct

--- memory_systems/conversator.py
+++ memory_systems/conversator.py
@@ -1,18 +1,13 @@
 from mlx_lm import load, generate
 
+from rag_systems.nederland_expert import chunks
+
 PDF_PATH = "./datasets/nederland.pdf"
 MODEL_NAME = "mlx-community/Llama-3.2-3B-Instruct"
-MARKER = "<|end|><|start|>assistant<|channel|>final<|message|>"
 DEFAULT_TOKENS = 1500
-TOKEN_LIMIT = 128000
+TOKEN_LIMIT = 300
 
-INTRO_PROMPT = "You are a conversational partner about all kind of different things. I want you to have a nice and friendly tone and answer in a nice and kind manner."
-RULES_PROMPT = f"""
-- Answer in the same language as the query.
-- Use the "MEMORY" I sent along to see what was sent by you and by the user.
-- If the message starts with "CHAT:" you send it, if it starts with "USER:" it was the user
-- The "QUESTION" is the question the user asks now.
-"""
+INTRO_PROMPT = "You are a friendly and helpful conversational assistant. Always answer in the same language as the user."
 
 CHAT_HISTORY = []
 CHAT_HISTORY_TEXT = ""
@@ -36,13 +31,12 @@ def convert_chat_history_to_text():
 
 def remove_first_messages():
     global CHAT_HISTORY
-    CHAT_HISTORY.pop()
+    CHAT_HISTORY = CHAT_HISTORY[2:]
 
 
 def calculate_token_length(query):
-    tokens = TOKENIZER.encode(INTRO_PROMPT) + TOKENIZER.encode(RULES_PROMPT) + TOKENIZER.encode(
+    tokens = TOKENIZER.encode(INTRO_PROMPT) + TOKENIZER.encode(
         CHAT_HISTORY_TEXT) + TOKENIZER.encode(query)
-    print(len(tokens))
     if len(tokens) > TOKEN_LIMIT:
         remove_first_messages()
         convert_chat_history_to_text()
@@ -50,34 +44,34 @@ def calculate_token_length(query):
 
 
 def build_prompt(query):
-    return f"""You are a friendly and helpful conversational assistant.
-Always answer in the same language as the user.
-DO NOT add to the conversation history, end after the answer
+    return f"""
+{INTRO_PROMPT}
 
------ Start of Conversation History -----
+Conversation history:
 {CHAT_HISTORY_TEXT}
------ End of Conversation History -----
 
-User: {query}
-Assistant:"""
+Current question:
+{query}
+
+Answer:
+"""
 
 
 def get_answer(query):
     global CHAT_HISTORY
     convert_chat_history_to_text()
-    print(CHAT_HISTORY)
     calculate_token_length(query)
     prompt = build_prompt(query)
-    print(prompt)
     result = generate(
         MODEL,
         TOKENIZER,
         prompt,
         max_tokens=DEFAULT_TOKENS,
     )
-    print(result)
     answer = result.strip()
-    CHAT_HISTORY.append(f"""User: {query}, Assistant: {answer}""")
+    CHAT_HISTORY.append(f"User: {query}")
+    CHAT_HISTORY.append(f"Assistant: {answer}\n")
+
     return answer
 
 
@@ -99,4 +93,3 @@ def run_conversator():
 
         answer = get_answer(query)
         print("\n✅ Answer:\n", answer)
-        print("\n---------------------------------------------\n")
